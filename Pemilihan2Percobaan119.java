221d3a5c3fa0b772253c8e3e99aba7b19c88a0fa
Nadyaaurora <nadyaaurora575@gmail.com>
2024-10-04 09:48:33 +0700

    Percobaan 1 modifikasi

--- Pemilihan2Percobaan119.java
+++ Pemilihan2Percobaan119.java
@@ -6,10 +6,18 @@ public class Pemilihan2Percobaan119 {
         int tahun;
         System.out.print("Masukkan tahun: ");
         tahun = input19.nextInt();
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
-            System.out.println("Tahun Kabisat");
-        } else
+            if ((tahun % 4) == 0) {
+                if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+                } else { 
+                if ((tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+                } else {
+                System.out.println("Bukan Tahun Kabisat");
+                }
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
